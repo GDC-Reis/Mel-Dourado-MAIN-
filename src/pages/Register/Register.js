090f1f5223e0e9dfe9d7aff007af13de1c01ab93
GDC-Reis <gustavodonisete11@gmail.com>
2024-04-07 13:18:58 -0300

    Commit realizado com as alterações no CSS do componente esqueci senha e da tela de cadastro para monitores de 1920px

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -44,66 +44,67 @@ const Register = () => {
     }, [authErro])
 
     return ( 
-        <div>
-            <h1 className='titulo_inicial'>Cadastra-se para realizar sua compra</h1>
-            
-            <form onSubmit={handleSubmit} className='form_sumbit'>
+        <section>
+            <div className='main_register'>
+                <h1 className='titulo_inicial'>Cadastra-se para realizar sua compra</h1>
                 
-                <label>
-                    <span>Nome: </span>
-                    <input 
-                        type="texte"
-                        name="displayName"
-                        required
-                        placeholder="Nome do usuário"
-                        value={displayName}
-                        onChange={(e) => setDisplayName(e.target.value)}/>
-                </label>
+                <form onSubmit={handleSubmit} className='form_sumbit'>
+                    
+                    <label>
+                        <span>Nome: </span>
+                        <input 
+                            type="texte"
+                            name="displayName"
+                            required
+                            placeholder="Nome do usuário"
+                            value={displayName}
+                            onChange={(e) => setDisplayName(e.target.value)}/>
+                    </label>
 
-                <label>
-                    <span>Email: </span>
-                    <input
-                        type="email"
-                        name="email"
-                        required
-                        placeholder="Email do usuário"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)} />
-                </label>
+                    <label>
+                        <span>Email: </span>
+                        <input
+                            type="email"
+                            name="email"
+                            required
+                            placeholder="Email do usuário"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)} />
+                    </label>
 
-                <label>
-                    <span>Senha: </span>
-                    <input
-                        type="password"
-                        name="password"
-                        required
-                        placeholder="Insira sua senha"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}/>
-                </label>
+                    <label>
+                        <span>Senha: </span>
+                        <input
+                            type="password"
+                            name="password"
+                            required
+                            placeholder="Insira sua senha"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}/>
+                    </label>
 
-                <label>
-                    <span>Confirmação de senha: </span>
-                    <input 
-                        type="password"
-                        name="password"
-                        required
-                        placeholder="Confirme sua senha"
-                        value={confirmPassword}
-                        onChange={(e) => setConfirmPassowrd(e.target.value)}
-                        />
-                </label>
+                    <label>
+                        <span>Confirmação de senha: </span>
+                        <input 
+                            type="password"
+                            name="password"
+                            required
+                            placeholder="Confirme sua senha"
+                            value={confirmPassword}
+                            onChange={(e) => setConfirmPassowrd(e.target.value)}
+                            />
+                    </label>
 
-                {!loading && <button className="btn">Cadastrar</button>}
-                {loading && (
-                    <button className="btn" disabled>Aguarde...</button>
-                )}
-                {error && <p className="error">{error}</p>}
+                    {!loading && <button className="btn">Cadastrar</button>}
+                    {loading && (
+                        <button className="btn" disabled>Aguarde...</button>
+                    )}
+                    {error && <p className="error">{error}</p>}
 
-            </form>
-        
-        <Footer/>
-        </div>
+                </form>
+            </div>
+            <Footer/>
+        </section>
      );
 }
  
